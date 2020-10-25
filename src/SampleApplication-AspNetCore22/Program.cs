c58f24a6e8b7c74548ef95a054bdd4e911e716e4
Lukas Angerer <executor.dev@gmail.com>
2020-10-25 11:57:13 +0100

    Default ASP.NET Core 2.2 MVC sample application with Ninject integration

---
+++ src/SampleApplication-AspNetCore22/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ninject;
+using Ninject.Web.Common.SelfHost;
+using Ninject.Web.WebApiCore.Hosting;
+
+namespace SampleApplication_AspNetCore22
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var options = new WebApiHostConfiguration()
+                .UseKestrel()
+                .UseStartup<Startup>();
+
+            var host = new NinjectSelfHostBootstrapper(CreateKernel, options);
+            host.Start();
+        }
+
+        public static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            return kernel;
+        }
+    }
+}
---
+++ src/SampleApplication-AspNetCore22/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ninject.Web.WebApiCore;
+using Ninject.Web.WebApiCore.Hosting;
+
+namespace SampleApplication_AspNetCore22
+{
+    public class Startup : WebApiStartupBase
+    {
+        public Startup(IConfiguration configuration, IServiceProviderFactory<NInjectServiceProviderBuilder> providerFactory)
+            : base(providerFactory)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+        }
+
+        public override void Configure(IApplicationBuilder app)
+        {
+            var env = app.ApplicationServices.GetRequiredService<IHostingEnvironment>();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
