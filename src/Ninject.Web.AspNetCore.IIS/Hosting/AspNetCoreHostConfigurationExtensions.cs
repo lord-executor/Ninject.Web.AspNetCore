4a09faee4c723251bb85412f69258218ffcab41f
Lukas Angerer <executor.dev@gmail.com>
2020-10-30 14:07:19 +0100

    Added separate assembly for IIS integration

---
+++ src/Ninject.Web.AspNetCore.IIS/Hosting/AspNetCoreHostConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace Ninject.Web.AspNetCore.Hosting
+{
+	public static class AspNetCoreHostConfigurationExtensions
+	{
+		public static T UseIIS<T>(this T config)
+			where T : IAspNetCoreHostConfiguration
+		{
+			config.ConfigureHostingModel(builder =>
+			{
+				builder
+					.UseIIS()
+					.UseIISIntegration();
+			});
+			return config;
+		}
+	}
+}
