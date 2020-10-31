bc4a25a5ce1d721d213bec9570c6ba1494797e43
Lukas Angerer <executor.dev@gmail.com>
2020-10-31 18:11:38 +0100

    Added diagnostic information to sample application home page

--- src/SampleApplication-AspNetCore22/Controllers/HomeController.cs
+++ src/SampleApplication-AspNetCore22/Controllers/HomeController.cs
@@ -1,29 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using SampleApplication_AspNetCore22.Models;
 
 namespace SampleApplication_AspNetCore22.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		private readonly IServer _server;
+		private readonly IServiceProvider _serviceProvider;
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public HomeController(IServer server, IServiceProvider serviceProvider)
+		{
+			_server = server;
+			_serviceProvider = serviceProvider;
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Index()
+		{
+			ViewData["EntryAssembly"] = Assembly.GetEntryAssembly().FullName;
+			ViewData["ProcessName"] = Process.GetCurrentProcess().ProcessName;
+			ViewData["Server"] = _server.GetType().FullName;
+			ViewData["ServiceProvider"] = _serviceProvider.GetType().FullName;
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
