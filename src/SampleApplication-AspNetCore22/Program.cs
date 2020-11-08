1165fd4480d1228dcf9868fe05ad2567e5b34461
Lukas Angerer <executor.dev@gmail.com>
2020-11-08 13:09:33 +0100

    Disabled automatic extension loading for StandardKernel

--- src/SampleApplication-AspNetCore22/Program.cs
+++ src/SampleApplication-AspNetCore22/Program.cs
@@ -65,7 +65,16 @@ namespace SampleApplication_AspNetCore22
 
 		public static IKernel CreateKernel()
 		{
-			var kernel = new StandardKernel();
+			var settings = new NinjectSettings();
+			// Unfortunately, in .NET Core projects, referenced NuGet assemblies are not copied to the output directory
+			// in a normal build which means that the automatic extension loading does not work _reliably_ and it is
+			// much more reasonable to not rely on that and load everything explicitly.
+			settings.LoadExtensions = false;
+
+			var kernel = new StandardKernel(settings);
+
+			kernel.Load(typeof(AspNetCoreHostConfiguration).Assembly);
+
 			return kernel;
 		}
 
