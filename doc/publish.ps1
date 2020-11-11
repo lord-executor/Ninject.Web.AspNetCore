43cc8a96d9333d81606f27c2cfde5303207767b2
Lukas Angerer <executor.dev@gmail.com>
2020-11-11 23:07:36 +0100

    Improved release process automation with scripting

--- doc/publish.ps1
+++ doc/publish.ps1
@@ -1,11 +1,31 @@
-$apiKey = "[API-Key]"
-$version = "[X.Y.Z]"
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $apiKey,
+    [string] $v2 = $null,
+    [string] $v3 = $null
+)
+
 $projects = (
     "Ninject.Web.AspNetCore",
     "Ninject.Web.AspNetCore.IIS",
     "Ninject.Web.AspNetCore.Httpsys"
 )
 
-$projects | ForEach-Object {
-    dotnet nuget push "src\$_\bin\Release\$_.$version.nupkg" -k $apiKey -s https://api.nuget.org/v3/index.json
+function Publish() {
+    param(
+        [string] $release,
+        [string] $version
+    )
+
+    $projects | ForEach-Object {
+        dotnet nuget push "src\$_\bin\Release$release\$_.$version.nupkg" -k $apiKey -s https://api.nuget.org/v3/index.json
+    }
+}
+
+if ($v2) {
+    Publish "22" $v2
+}
+
+if ($v3) {
+    Publish "31" $v3
 }
--- doc/release-process.md
+++ doc/release-process.md
@@ -2,13 +2,11 @@
 Build and package the project with the relevant _Release_ configuration - `Release22` for the ASP.NET Core 2.2 branch or `Release31` for the ASP.NET Core 3.x branch.
 
 ```
-dotnet build .\src\Ninject.Web.AspNetCore.sln -c Release22
-dotnet pack .\src\Ninject.Web.AspNetCore.sln -c Release22
-```
-
-```
-dotnet build .\src\Ninject.Web.AspNetCore.sln -c Release31
-dotnet pack .\src\Ninject.Web.AspNetCore.sln -c Release31
+dotnet pack .\src\Ninject.Web.AspNetCore.sln -c Release22 -p:Version="2.2.X"
+dotnet pack .\src\Ninject.Web.AspNetCore.sln -c Release31 -p:Version="3.0.X"
 ```
 
 Publish all three packages with the `publish.ps1` script, replacing the API key and versions as needed.
+```
+.\doc\publish.ps1 -apiKey "NuGet APIKey" -v2 "2.2.X" -v3 "3.0.X"
+```
