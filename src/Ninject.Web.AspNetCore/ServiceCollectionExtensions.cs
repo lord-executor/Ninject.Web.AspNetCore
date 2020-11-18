173ec264cb47502be172dbb206a76526e55fccf6
DominicUllmann <dominic_ullmann@hotmail.com>
2020-11-18 19:45:45 +0100

    cleanup comment as we found a way to construct the IServiceProvider in the same way for all asp.net versions

--- src/Ninject.Web.AspNetCore/ServiceCollectionExtensions.cs
+++ src/Ninject.Web.AspNetCore/ServiceCollectionExtensions.cs
@@ -9,8 +9,7 @@ namespace Ninject.Web.AspNetCore
 	public static class ServiceCollectionExtensions
 	{
 		/// <summary>
-		/// Adds the <see cref="NinjectServiceProviderFactory"/> to the service collection. ONLY FOR PRE-ASP.NET 3.0 HOSTING. THIS WON'T WORK
-		/// FOR ASP.NET CORE 3.0+ OR GENERIC HOSTING.
+		/// Adds the <see cref="NinjectServiceProviderFactory"/> to the service collection. 
 		/// </summary>
 		/// <param name="services">The service collection to add the factory to.</param>
 		/// <param name="configurationAction">Action on a <see cref="ContainerBuilder"/> that adds component registrations to the container.</param>
