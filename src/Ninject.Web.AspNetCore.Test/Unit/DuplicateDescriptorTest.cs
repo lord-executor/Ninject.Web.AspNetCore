9a6e0ea72df727666b7a33f501da7dcd7b728534
Lukas Angerer <executor.dev@gmail.com>
2020-12-18 10:49:42 +0100

    Expanded duplicate descriptors test cases

--- src/Ninject.Web.AspNetCore.Test/Unit/DuplicateDescriptorTest.cs
+++ src/Ninject.Web.AspNetCore.Test/Unit/DuplicateDescriptorTest.cs
@@ -53,13 +53,21 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 			);
 		}
 
-		[Fact]
-		public void BindingsFromKernel_ResolveWarrior_FailsWithActivationException()
+		[Theory]
+		[MemberData(nameof(ResolverData))]
+		public void BindingsFromKernel_ResolveWarrior_FailsWithActivationException(Resolver resolver)
 		{
 			var kernel = CreateDuplicateBindings();
 
-			Action action = () => kernel.Get<IWarrior>();
-			action.Should().Throw<ActivationException>().WithMessage("Error activating IWarrior*");
+			if (resolver.ResolveIsOptional)
+			{
+				resolver.Resolve<IWarrior>(kernel).Should().BeNull();
+			}
+			else
+			{
+				Action action = () => resolver.Resolve<IWarrior>(kernel);
+				action.Should().Throw<ActivationException>().WithMessage("Error activating IWarrior*");
+			}
 		}
 
 		[Fact]
@@ -69,6 +77,51 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 
 			Action action = () => kernel.Get<IWarrior>(metadata => metadata.Get(nameof(Knight), false));
 			action.Should().Throw<ActivationException>().WithMessage("Error activating IWeapon*");
+		}
+
+		[Theory]
+		[MemberData(nameof(ResolverData))]
+		public void BindingsMixedWarriorDescriptors_ResolveWarrior_FailsWithActivationException(Resolver resolver)
+		{
+			var collection = new ServiceCollection();
+			collection.Add(new ServiceDescriptor(typeof(IWarrior), _ => new Ninja("Shadow"), ServiceLifetime.Transient));
+			collection.Add(new ServiceDescriptor(typeof(IWarrior), typeof(Knight), ServiceLifetime.Transient));
+			var kernel = CreateKernel(collection);
+			kernel.Bind<IWeapon>().To<Lance>();
+			kernel.Bind<IWeapon>().To<Longsword>().WithMetadata(nameof(Longsword), true);
+
+			// IWarrior is resolved to Knight because of the "latest" binding, but the two weapon bindings are equivalent
+			// and lead to an activation exception
+			if (resolver.ResolveIsOptional)
+			{
+				resolver.Resolve<IWarrior>(kernel).Should().BeNull();
+			}
+			else
+			{
+				Action action = () => resolver.Resolve<IWarrior>(kernel);
+				action.Should().Throw<ActivationException>().WithMessage("Error activating IWeapon*");
+			}
+		}
+
+		[Fact]
+		public void BindingsMixedWeaponDescriptors_ResolveWarrior_FailsWithActivationException()
+		{
+			var collection = new ServiceCollection();
+			collection.Add(new ServiceDescriptor(typeof(IWeapon), typeof(Lance), ServiceLifetime.Transient));
+			collection.Add(new ServiceDescriptor(typeof(IWeapon), typeof(Longsword), ServiceLifetime.Transient));
+			var kernel = CreateKernel(collection);
+			kernel.Bind<IWarrior>().ToConstant(new Ninja("Shadow"));
+			kernel.Bind<IWarrior>().To<Knight>().WithMetadata(nameof(Knight), true);
+
+			// IWarrior is cannot be resolved because the two Kernel bindings are equivalent
+			Action action = () => kernel.Get<IWarrior>();
+			action.Should().Throw<ActivationException>().WithMessage("Error activating IWarrior*");
+
+			// IWeapon binding still works if IWarrior binding is constrained to only match one binding
+			var warrior = kernel.Get<IWarrior>(metadata => metadata.Get(nameof(Knight), false));
+			warrior.Should().NotBeNull();
+			warrior.Should().BeOfType<Knight>();
+			(warrior as Knight).Weapon.Should().BeOfType<Longsword>();
 		}
 
 		private IKernel CreateDuplicateDescriptors()
@@ -84,7 +137,7 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 
 		private IKernel CreateDuplicateBindings()
 		{
-			var kernel = new AspNetCoreKernel(new NinjectSettings() { LoadExtensions = false });
+			var kernel = CreateKernel(new ServiceCollection());
 			kernel.Bind<IWarrior>().ToConstant(new Ninja("Shadow"));
 			kernel.Bind<IWarrior>().To<Knight>().WithMetadata(nameof(Knight), true);
 			kernel.Bind<IWeapon>().To<Lance>();
@@ -96,23 +149,31 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 		public enum ResolveType
 		{
 			ServiceProvider,
-			Kernel,
+			ServiceProviderRequired,
+			KernelGet,
+			KernelTryGet,
 		}
 
 		public class Resolver
 		{
-			private readonly ResolveType _resolveType;
+			public ResolveType ResolveType { get; }
+
+			public bool ResolveIsOptional => ResolveType == ResolveType.ServiceProvider || ResolveType == ResolveType.KernelTryGet;
 
 			public Resolver(ResolveType resolveType)
 			{
-				_resolveType = resolveType;
+				ResolveType = resolveType;
 			}
 			public T Resolve<T>(IKernel kernel)
 			{
-				switch (_resolveType)
+				switch (ResolveType)
 				{
 					case ResolveType.ServiceProvider:
 						return kernel.Get<IServiceProvider>().GetService<T>();
+					case ResolveType.ServiceProviderRequired:
+						return kernel.Get<IServiceProvider>().GetRequiredService<T>();
+					case ResolveType.KernelTryGet:
+						return kernel.TryGet<T>();
 
 					default:
 						return kernel.Get<T>();
@@ -121,9 +182,10 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 
 			public IEnumerable<T> ResolveAll<T>(IKernel kernel)
 			{
-				switch (_resolveType)
+				switch (ResolveType)
 				{
 					case ResolveType.ServiceProvider:
+					case ResolveType.ServiceProviderRequired:
 						return kernel.Get<IServiceProvider>().GetServices<T>();
 
 					default:
@@ -135,7 +197,9 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 		public static TheoryData<Resolver> ResolverData => new TheoryData<Resolver>
 		{
 			{ new Resolver(ResolveType.ServiceProvider) },
-			{ new Resolver(ResolveType.Kernel) },
+			{ new Resolver(ResolveType.ServiceProviderRequired) },
+			{ new Resolver(ResolveType.KernelTryGet) },
+			{ new Resolver(ResolveType.KernelGet) },
 		};
 	}
 }
