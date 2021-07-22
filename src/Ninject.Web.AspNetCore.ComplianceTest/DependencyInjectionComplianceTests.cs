4697cb531e50e19a3c5a63703bdfd0ab0180680c
Lukas Angerer <executor.dev@gmail.com>
2021-07-22 10:19:07 +0200

    Added compliance tests for Microsoft.Extensions.DependencyInjection

---
+++ src/Ninject.Web.AspNetCore.ComplianceTest/DependencyInjectionComplianceTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.DependencyInjection;
+using Ninject.Web.AspNetCore;
+using System;
+using Xunit;
+
+namespace Ninject.Web.AspNetCore.ComplianceTests
+{
+	/// <summary>
+	/// See https://github.com/dotnet/runtime/tree/main/src/libraries/Microsoft.Extensions.DependencyInjection.Specification.Tests/src - the dotnet/runtime
+	/// project which contains the dependency injection library code also contains a set of "compliance tests" that can be run against a potential alternative
+	/// implementation to check if it is compliant. This class here is doing just that.
+	/// 
+	/// The project also contains a separate test project that includes these compliance tests for a set of compliant third party DI implementations like
+	/// Autofac and Lightinject under https://github.com/dotnet/runtime/tree/main/src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.External.Tests.
+	/// 
+	/// All of this is part of the https://github.com/dotnet/runtime/blob/main/src/libraries/Microsoft.Extensions.DependencyInjection/Microsoft.Extensions.DependencyInjection.sln
+	/// solution of dotnet/runtime.
+	/// </summary>
+	public class DependencyInjectionComplianceTests : Microsoft.Extensions.DependencyInjection.Specification.DependencyInjectionSpecificationTests
+	{
+		protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+		{
+			var kernel = new AspNetCoreKernel();
+			// The compliance tests assume that a scoped service can always be resolved
+			kernel.Settings.SetMissingRequestScopeBehavior(MissingRequestScopeBehaviorType.UseKernel);
+
+			var factory = new NinjectServiceProviderFactory(kernel);
+
+			return factory.CreateBuilder(serviceCollection).Build();
+		}
+	}
+}
