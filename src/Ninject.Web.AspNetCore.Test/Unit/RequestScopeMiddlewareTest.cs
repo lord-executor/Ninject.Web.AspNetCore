edf3bd7f4da1f6defe7e93e45b03cb0729c4d9cd
Lukas Angerer <lukas.angerer@elca.ch>
2021-10-14 16:22:14 +0200

    Updated RequestScopeMiddleware to NOT implement IMiddleware

--- src/Ninject.Web.AspNetCore.Test/Unit/RequestScopeMiddlewareTest.cs
+++ src/Ninject.Web.AspNetCore.Test/Unit/RequestScopeMiddlewareTest.cs
@@ -12,18 +12,21 @@ namespace Ninject.Web.AspNetCore.Test.Unit
 		[Fact]
 		public void Middleware_WhenInvoked_CreatesAndDestroysRequestScope()
 		{
-			var middleware = new RequestScopeMiddleware();
-			var httpContextMock = new Mock<HttpContext>();
-
-			RequestScope.Current.Should().BeNull();
-			middleware.InvokeAsync(httpContextMock.Object, _ =>
+			var callCount = 0;
+			var middleware = new RequestScopeMiddleware(_ =>
 			{
 				// make sure that there is actually some asynchronicity in the pipeline
 				return Task.Delay(200).ContinueWith(_ =>
 				{
+					callCount++;
 					RequestScope.Current.Should().NotBeNull();
 				});
-			}).Wait();
+			});
+			var httpContextMock = new Mock<HttpContext>();
+
+			RequestScope.Current.Should().BeNull();
+			middleware.InvokeAsync(httpContextMock.Object).Wait();
+			callCount.Should().Be(1);
 			RequestScope.Current.Should().BeNull();
 		}
 	}
