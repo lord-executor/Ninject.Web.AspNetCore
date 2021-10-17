224be64bf4f634324271d6e43b6bc2ade5872bba
Lukas Angerer <executor.dev@gmail.com>
2021-10-17 11:18:31 +0200

    Using custom linked queue implementation for tracking instances

--- src/Ninject.Web.AspNetCore/Components/DisposalManager.cs
+++ src/Ninject.Web.AspNetCore/Components/DisposalManager.cs
@@ -1,10 +1,11 @@
 ﻿using Ninject.Activation;
+using Ninject.Activation.Caching;
 using Ninject.Components;
 using Ninject.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Xml.Linq;
 
 namespace Ninject.Web.AspNetCore.Components
 {
@@ -21,15 +22,15 @@ namespace Ninject.Web.AspNetCore.Components
 	/// </summary>
 	public class DisposalManager : NinjectComponent, IDisposalManager
 	{
-		private readonly LinkedList<ReferenceEqualWeakReference> activeInstances = new LinkedList<ReferenceEqualWeakReference>();
+		private readonly ListNode _head = new ListNode(null);
 		private readonly AsyncLocal<IDisposalCollectorArea> _area = new AsyncLocal<IDisposalCollectorArea>();
-		private readonly object _collectionLock = new object();
+		private readonly object _disposalLock = new object();
 
 		public void AddInstance(InstanceReference instanceReference)
 		{
-			lock (_collectionLock)
+			lock (_head)
 			{
-				activeInstances.AddLast(new ReferenceEqualWeakReference(instanceReference.Instance));
+				_head.Insert(new ListNode(new ReferenceEqualWeakReference(instanceReference.Instance)));
 			}
 		}
 
@@ -48,6 +49,70 @@ namespace Ninject.Web.AspNetCore.Components
 			return new InnerDisposalArea(_area.Value);
 		}
 
+		/// <summary>
+		/// To help with concurrency of creating and disposing services in a multi-threaded environment, we use a single-lined
+		/// "queue" with a dedicated head that does not hold a value where we can add items. This means that we can still add
+		/// new items to the queue while we are also cleaning up the tail of the queue since we only have to lock the head
+		/// while removing items directly following the head.
+		/// </summary>
+		private class ListNode
+		{
+			public ReferenceEqualWeakReference Value { get; }
+			public ListNode Next { get; set; }
+
+			public ListNode(ReferenceEqualWeakReference value)
+			{
+				Value = value;
+			}
+
+			public void Insert(ListNode node)
+			{
+				node.Next = Next;
+				Next = node;
+			}
+
+			/// <summary>
+			/// This is a (hopefully) clever algorithm that removes all nodes _directly following_ the current one
+			/// that are either already dead or need to be disposed. At the end, <see cref="Next"/> will point to
+			/// the next queue entry that is still alive and does not need to be disposed.
+			/// </summary>
+			public ListNode PruneAndDispose(Func<ReferenceEqualWeakReference, bool> disposalCondition)
+			{
+				if (Next == null)
+				{
+					// nothing to do here by definition
+					return null;
+				}
+
+				var current = Next;
+
+				var needsDisposal = disposalCondition(current.Value);
+				// It is always possible that a service went out of scope that wasn't managed by an IServiceScope,
+				// so we also prune dead service references from the queue here.
+				while (needsDisposal || !current.Value.IsAlive)
+				{
+					if (needsDisposal)
+					{
+						(current.Value.Target as IDisposable)?.Dispose();
+					}
+					current = current.Next;
+					if (current == null)
+					{
+						// early exit if we have reached the end of the queue - everything from "this" to the end
+						// of the very end of the queue can be removed.
+						break;
+					}
+					needsDisposal = disposalCondition(current.Value);
+				}
+
+				if (Next != current)
+				{
+					Next = current;
+				}
+
+				return current;
+			}
+		}
 
 		#region IDisposalCollectorArea Implementations
 
@@ -59,7 +124,14 @@ namespace Ninject.Web.AspNetCore.Components
 		private class OrderedAggregateDisposalArea : IDisposalCollectorArea
 		{
 			private readonly DisposalManager _manager;
-			private readonly HashSet<ReferenceEqualWeakReference> _disposals = new HashSet<ReferenceEqualWeakReference>();
+			/// <summary>
+			/// The disposal area should be a very limited scope (see <see cref="NinjectServiceScope.Dispose(bool)"/>)
+			/// which should allow us to avoid having to create yet another weak reference for every service that is being
+			/// disposed. At the end of the <see cref="Dispose"/>, all of the (hard) references that are held in the
+			/// <see cref="_disposals"/> set go out of scope and have been disposed (if they are disposable). We can still
+			/// find the correct entry in the hash set by using <see cref="UnwrappingReferenceEqualityComparer"/>
+			/// </summary>
+			private readonly HashSet<object> _disposals = new HashSet<object>(new UnwrappingReferenceEqualityComparer());
 
 			public OrderedAggregateDisposalArea(DisposalManager manager)
 			{
@@ -73,38 +145,30 @@ namespace Ninject.Web.AspNetCore.Components
 					return;
 				}
 
-				lock (_manager._collectionLock)
+				lock (_manager._disposalLock)
 				{
-					var node = _manager.activeInstances.Last;
-					if (node == null)
-					{
-						return;
-					}					
+					ListNode current;
 
-					do
+					// while we are operating on the _head, it must be locked because of potential concurrency with AddInstance,
+					// but as soon as we have moved past the head, we only need to keep the tail lock.
+					lock (_manager._head)
 					{
-						var current = node;
-						node = node.Previous;
-						if (_disposals.Contains(current.Value))
-						{
-							_manager.activeInstances.Remove(current);
-							(current.Value.Target as IDisposable)?.Dispose();
-						}
-						else if (!current.Value.IsAlive)
-						{
-							// It is always possible that a service went out of scope that wasn't managed by an IServiceScope,
-							// so we also prune dead service references from the list here.
-							_manager.activeInstances.Remove(current);
-						}
-					} while (node != null);
+						current = _manager._head.PruneAndDispose(weakReference => _disposals.Contains(weakReference));
+					}
 
-					_manager._area.Value = null;
+					while (current != null)
+					{
+						current = current.PruneAndDispose(weakReference => _disposals.Contains(weakReference));
+					}
 				}
+
+				_manager._area.Value = null;
+				_disposals.Clear();
 			}
 
 			public void Register(InstanceReference instanceReference)
 			{
-				_disposals.Add(new ReferenceEqualWeakReference(instanceReference.Instance));
+				_disposals.Add(instanceReference.Instance);
 			}
 		}
 
@@ -123,7 +187,8 @@ namespace Ninject.Web.AspNetCore.Components
 
 		/// <summary>
 		/// When disposal collection areas are nested, everything is delegated to the outermost area
-		/// and services are only disposed with the disposal of the outermost <see cref="OrderedAggregateDisposalArea"/>.
+		/// and services are only disposed with the disposal of the outermost <see cref="OrderedAggregateDisposalArea"/>
+		/// because only there do we know all the services that need to be disposed.
 		/// </summary>
 		private class InnerDisposalArea : IDisposalCollectorArea
 		{
---
+++ src/Ninject.Web.AspNetCore/Components/UnwrappingReferenceEqualityComparer.cs
@@ -0,0 +1,22 @@
+﻿using Ninject.Infrastructure;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Ninject.Web.AspNetCore.Components
+{
+	public class UnwrappingReferenceEqualityComparer : IEqualityComparer<object>
+	{
+		public new bool Equals(object x, object y)
+		{
+			// micro-optimization: we are checking "y" first, because we know that in DisposalManager, x will be the "plain" object
+			// and y will be the weak reference.
+			return y is ReferenceEqualWeakReference weakY && weakY.Equals(x) || x is ReferenceEqualWeakReference weakX && weakX.Equals(y);
+		}
+
+		public int GetHashCode(object obj)
+		{
+			var weakReference = obj as ReferenceEqualWeakReference;
+			return weakReference?.GetHashCode() ?? RuntimeHelpers.GetHashCode(obj);
+		}
+	}
+}
