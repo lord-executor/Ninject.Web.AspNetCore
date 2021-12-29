d8bfeb6f5e916293eaa8238cad1563948e232d1f
Lukas Angerer <executor.dev@gmail.com>
2021-12-29 11:45:49 +0100

    Converted Program to a class

--- src/SampleBlazorApplication/Program.cs
+++ src/SampleBlazorApplication/Program.cs
@@ -2,27 +2,34 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SampleBlazorApplication.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace SampleBlazorApplication;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		builder.Services.AddRazorPages();
+		builder.Services.AddServerSideBlazor();
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Error");
+		}
+
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.MapBlazorHub();
+		app.MapFallbackToPage("/_Host");
+
+		app.Run();
+	}
 }
-
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
